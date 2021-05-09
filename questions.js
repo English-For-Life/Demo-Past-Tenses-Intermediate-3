737dbd1c528b805ba0c75d93c663931531f6e6ab
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 21:19:50 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"In the past, land travels _____ very long and they _____ very tiring.",
+        options:["used to take / were", "had been taking / had been", "had taken / were", "take / have been"],
+        answer: 0
+    },
+    {
+     q:"They _____ the whole building by the time the fire _____ to all the floors.",
+        options:["evacuated/had spread", "had evacuated / spread", "were evacuating / has spread", "evacuated / was spreading"],
+        answer: 1   
+    },
+    {
+        q:"Liam _____ for ten hours, but he still _____ half of the distance by then.",
+        options:["has been driving / didn't cover", "was driving / didn't cover", "had been driving / hadn't covered", "is driving / hasn't been covering"],
+        answer: 2
+    },
+    {
+        q:"I _____ to be able to concentrate on my work because I _____ enough sleep last night.",
+        options:["don't seem / haven't got", "didn't seem / am not getting", "didn't seem / don't get", "don't seem / didn't get"],
+        answer: 3
+    },
+    {
+        q:"When we _____ again, Steve and I _____ each other for more than fifteen years.",
+        options:["met/hadn't seen", "are meeting / haven't seen", "met / don't see", "have met / didn't see"],
+        answer: 0
+    }
+]
